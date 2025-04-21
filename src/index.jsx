7f606fe0245bad7905532ce954330f424f759418
Ianbarbosa1 <iancosta.tec@gmail.com>
2025-04-21 20:08:02 -0300

    teste da imagem

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import ironman from './assets/imagens/i-am-iron-man_1572368483.jpg'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +11,7 @@ function App() {
       <h1>Testando o css no react</h1>
       <h2>{count}</h2>
       <button onClick={click}>Adicionar</button>
+      <img src={ironman} alt="homem de ferro" className='imagem'/>
     </section>
   )
 }
