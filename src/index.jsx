c3795260b60911b5b3d68bd5dd6d26afe4f1d218
Ianbarbosa1 <iancosta.tec@gmail.com>
2025-04-21 20:48:03 -0300

    testando mudanças

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
+import { Titulo } from './componentes/titulo'
 import ironman from './assets/imagens/i-am-iron-man_1572368483.jpg'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -8,7 +10,7 @@ function App() {
   }
   return (
     <section className='container'>
-      <h1>Testando o css no react</h1>
+      <Titulo/>
       <h2>{count}</h2>
       <button onClick={click}>Adicionar</button>
       <img src={ironman} alt="homem de ferro" className='imagem'/>
